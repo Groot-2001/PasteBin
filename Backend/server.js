923eb00e2445405b3cbca06ca6a981cc7f68c5cf
Sajid Shaikh <shaikhsajid11112000@gmail.com>
2023-02-25 12:12:44 +0530

    Added setup files

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
-const status = require("./controller/get_status"); 
+const status = require("./controller/get_status");
+const PORT = 3001;
+var bodyParser = require('body-parser')
 
-app.get('/',status);
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.listen(3001);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers: Content-Type, Custom-Header");
+  next();
+});
+
+app.get('/', status);
+
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+})
