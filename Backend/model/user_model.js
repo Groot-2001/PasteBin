3915391f59cfccc39dd3804737742f6a66a8618f
Groot-2001 <shivasilmawala841@gmail.com>
2023-03-08 14:12:41 +0530

    user model modified

--- Backend/model/user_model.js
+++ Backend/model/user_model.js
@@ -4,24 +4,24 @@ const Schema = mongoose.Schema;
 const UserSchema= new Schema({
     username:{
         type:String,
+        required:true,
         unique:true
     },
     email:{
         type:String,
         unique:true,
+        match: /.+\@.+\..+/,
+        required:true
     },
     name:{
         type:String,
+        required:true,
     },
     password:{
         type:String,
-    },
-    paste_data:[
-        {
-            type: Schema.Types.ObjectId,
-            ref:"Paste_Model"
-        }
-    ]
+        required:true,
+        min:8
+    }
 },{timestamps:true});
 
 module.exports = mongoose.model('User_Model',UserSchema);
